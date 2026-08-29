face4598dabff9d781799b91c62784ce8e9e9d21
astromargoinfo-ux <astromargo.info@gmail.com>
2026-08-29 16:54:46 +0300

    Update api.js

--- api.js
+++ api.js
@@ -91,5 +91,12 @@ res.setHeader(
 
 return res.status(200).
 json(result);
-} catch (error) { return res.status(400).json({ error: error.message, });
+} catch (error) { return res.
+  status(400)
+  json({ 
+    error: error.message,
+  });
+  }
+  }
+                 
    } }
